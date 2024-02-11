f55b4a25a349c8e90779ce31f37536efb548e054
CodeMehringer <153135304+CodeMehringer@users.noreply.github.com>
2024-02-10 22:22:08 -0600

    Custom JavaScript Part 2

--- scripts.js
+++ scripts.js
@@ -1,16 +1,29 @@
 console.log('javascript connected!');
     
 const carousel = new bootstrap.Carousel('#homeCarousel', {
-    interval: 2000,
+    interval: 5000,
     pause: false
 })
 
 
 //pause button action when clicked on carousel
-const carouselPause = document.getElementById('carouselPause');
-carouselPause.addEventListener('click', function() {
-    console.log('pausing the carousel');
-    carousel.pause();
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
+
+carouselButton.addEventListener('click', function() {
+    //the if else statements were added here when the play button was removed. 
+    if (faIcon.classList.contains('fa-pause')){
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    }else{
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
+    //no longer pausing the carousel with the pause/play buttons so this is removed
+//    console.log('pausing the carousel');
+//    carousel.pause();
 })
 
 //play button action when clicked on carousel, cycle through images
