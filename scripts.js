4aba08bccb4c240cc053a7d95a7d74cb3d9940d4
CodeMehringer <153135304+CodeMehringer@users.noreply.github.com>
2024-02-18 11:12:44 -0600

    final copy of weather updates

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
 console.log('javascript connected!');
-    
+
 const carousel = new bootstrap.Carousel('#homeCarousel', {
     interval: 5000,
     pause: false
 })
-
 
 //pause button action when clicked on carousel
 const carouselButton = document.getElementById('carouselButton');
@@ -26,9 +26,41 @@ carouselButton.addEventListener('click', function() {
 //    carousel.pause();
 })
 
-//play button action when clicked on carousel, cycle through images
-const carouselPlay = document.getElementById('carouselPlay');
-carouselPlay.addEventListener('click', function() {
-    console.log('cycle the carousel');
-    carousel.cycle();
-})
+//play button action when clicked on carousel, cycle through images FIX THIS CODE 
+//const carouselPlay = document.getElementById('carouselPlay');
+//carouselPlay.addEventListener('click', function() {
+//    console.log('cycle the carousel');
+//    carousel.cycle();
+//})
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    let city = "breckenridge";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+        displayWeather(data);
+        } catch(error) {
+            console.error("An unexpected error occured", error);
+        }
+        
+    }
+    fetchWeather();
+    
+    function displayWeather(json) {
+        let weatherImg = document.createElement('img');
+        weatherImg.src = `https://openweathermap.org/img/w/${json.weather[0].icon}.png`;
+        document.getElementById('weather-icon').appendChild(weatherImg);
+
+        let temp = document.createElement('h4');
+        temp.textContent = `${json.main.temp} \u00B0"|  Feels like: ${json.main.feels_like}`;
+        document.getElementById('weather-temp').appendChild(temp);
+
+
+        let description = document.createElement('h4');
+        description.textContent = json.weather[0].description;
+        document.getElementById('weather-description').appendChild(description);
+    
+    }
